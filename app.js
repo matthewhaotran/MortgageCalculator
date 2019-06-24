7fbcea8eb2a5dd0b2a1c45873a0526396bf7f029
Matthew Tran <matthewtran@matthews-air.lan>
2019-06-23 22:05:47 -0700

    add button

---
+++ app.js
@@ -0,0 +1,7 @@
+let principal = $('#principal').val();
+let rate = $('#rate').val();
+let period = $('#period').val();
+$( "#calculate" ).click(function() {
+    let principal = $('#principal').val();
+    alert( principal );
+  });
