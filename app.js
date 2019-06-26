ea31177c029624cb94d00a076ce4b1993cd5a90f
Matthew Tran <matthew@Matthews-MacBook-Pro.local>
2019-06-26 16:47:38 -0700

    start formula

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 let principal = $('#principal').val();
 let rate = $('#rate').val();
-let period = $('#period').val();
-$( "#calculate" ).click(function() {
-    let principal = $('#principal').val();
-    alert( principal );
-  });
+let years = $('#years').val();
+$("#calculate").click(function () {
+  let principal = $('#principal').val();
+  alert(principal);
+  principal = $('#balance').val();
+  rate = $('#rate').val();
+  years = $('#years').val();
+  period = 12;
+  monthlyInterestRate = (rate / 100) / period;
+  numberOfPayments = loanTerms * period;
+  compoundedInterestRate = Math.pow((1 + monthlyInterestRate), numberOfPayments);
+  interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
+  monthlyPayment = principal * interestQuotient;
+});
